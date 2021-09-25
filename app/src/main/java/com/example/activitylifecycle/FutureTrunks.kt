a909d6625c9cd9284ce685beee5bfa539625ba7e
rickyshiv <rickyshivd10@gmail.com>
2021-09-25 15:29:00 +0530

    comment added

--- app/src/main/java/com/example/activitylifecycle/FutureTrunks.kt
+++ app/src/main/java/com/example/activitylifecycle/FutureTrunks.kt
@@ -7,5 +7,6 @@ class FutureTrunks : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_future_trunks)
+        // futuretrunks
     }
 }
